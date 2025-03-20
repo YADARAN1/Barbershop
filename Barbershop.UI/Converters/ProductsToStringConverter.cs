c677bf58d1455adaeea1340364726f617ec1aee6
YADARAN1 <danil-tkachenko-01@list.ru>
2025-03-21 00:33:58 +0500

    init project

---
+++ Barbershop.UI/Converters/ProductsToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using Barbershop.Contracts.Models;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Barbershop.UI.Converters
+{
+    public class ProductsToStringConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is List<ProductDto> productDtos)
+            {
+                if (productDtos.Count == 0)
+                    return "Товары не выбраны";
+
+                var productNames = productDtos.Select(dto => dto.Name);
+
+                return string.Join(", ", productNames);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
